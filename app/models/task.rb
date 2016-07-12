0e9dca44b7e234ada3f92fa84688485c565c4aa1
achernyak <artem@ikayzo.com>
2016-07-12 08:49:23 -0500

    using resources

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,22 +1,18 @@
-class Task
-  attr_accessor :size, :completed_at
+class Task < ActiveRecord::Base
 
-  def initialize(options = {})
-    mark_completed(options[:completed_at]) if options[:completed_at]
-    @size = options[:size]
-  end
+  belongs_to :project
 
   def mark_completed(date = nil)
-    @completed_at = (date || Time.current)
+    self.completed_at = (date || Time.current)
   end
 
   def complete?
-    @completed_at.present?
+    self.completed_at.present?
   end
 
   def part_of_velocity?
     return false unless complete?
-    completed_at > Project.velocity_length_in_days.days.ago
+    self.completed_at > Project.velocity_length_in_days.days.ago
   end
 
   def points_toward_velocity
